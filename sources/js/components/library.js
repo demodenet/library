caa9a1a847ceee98c881e7695bdb46caa684ed04
Sylwia Rybicka <sr@k2.pl>
2016-12-07 13:11:22 +0100

    zapomniany plik

---
+++ sources/js/components/library.js
@@ -0,0 +1,47 @@
+import Book from "./book";
+
+class Library {
+    constructor() {
+        this.bookshelf = [];
+        this.$ui = $('.ui');
+
+        this.getBooks()
+            .then(this.processBooks.bind(this))
+            .then(this.allowUI.bind(this));
+
+        this.bindUI();
+    }
+
+    allowUI() {
+        this.$ui.show();
+    }
+
+    bindUI() {
+        $('.js-list-books').on('click', this.listBooksTitle.bind(this));
+    }
+
+    getBooks() {
+        return $.ajax({
+            dataType: 'json',
+            url: '../js/data/bookList.json'
+        });
+    }
+
+    processBooks(data) {
+        data.books.forEach(this.assignBook.bind(this));
+    }
+
+    assignBook(obj) {
+        this.bookshelf.push(new Book(obj));
+    }
+
+    listBooksTitle() {
+        console.log('Posiadam książki: ');
+        this.bookshelf.forEach(book => {
+            console.log(book.getTitle());
+        });
+    }
+}
+
+
+export default Library;
